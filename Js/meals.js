70b8fe1e84b46601391fa72fbad5a63977d7c7a5
Fawzia Ruma <fawziaruma0652@gmail.com>
2023-03-17 09:24:02 +0600

    second commit

--- Js/meals.js
+++ Js/meals.js
@@ -3,7 +3,7 @@ const loadMeals = (searchText) =>{
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displayMeals(data))
+    .then(data => displayMeals(data.meals))
 }
 
 
@@ -14,10 +14,38 @@ const displayMeals = meals =>{
     mealsContainer.innerText = ''
    meals.forEach(meal => {
         console.log(meal);
+        const mealDiv = document.createElement('div')
+        mealDiv.classList.add('col')
+        mealDiv.innerHTML = `
+        <div onClick='loadMealDetails(${meal.idMeal})' class="card" >
+        <img src="${meal.strMealThumb}" class="card-img-top  " alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${meal.strMeal}</h5>
+          <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+        </div>
+      </div>
+        `;
+        mealsContainer.appendChild(mealDiv)
    });
 }
 const searchMeal = () =>{
-    const searchText = document.getElementById('search-field').value 
+    const searchField = document.getElementById('search-field')
+
+    const searchText =searchField.value 
     loadMeals(searchText)
+    searchField.value = ''
+   
+
+
 
 }
+const loadMealDetails = (idMeal) =>{
+    console.log(idMeal);
+    const url = ` https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    fetch(url)
+    .then(res =>res.json())
+    .then(data => console.log(data.meals[0]))
+}
+const displayMealsDetails = meal =>{}
+
+loadMeals(' ')
